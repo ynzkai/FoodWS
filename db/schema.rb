5d23b0da5d04d59f6ded518872a0575a4ccd7dbc
ynzkai <ynzkai@gmail.com>
2016-02-14 02:21:05 -0800

    add comment and improve style

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160212090344) do
+ActiveRecord::Schema.define(version: 20160214083540) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "content",    limit: 65535
@@ -36,12 +36,13 @@ ActiveRecord::Schema.define(version: 20160212090344) do
   end
 
   create_table "posts", force: :cascade do |t|
-    t.string   "title",      limit: 255
-    t.text     "content",    limit: 65535
-    t.integer  "topic_id",   limit: 4
-    t.integer  "user_id",    limit: 4
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
+    t.string   "title",          limit: 255
+    t.text     "content",        limit: 65535
+    t.integer  "topic_id",       limit: 4
+    t.integer  "user_id",        limit: 4
+    t.datetime "created_at",                               null: false
+    t.datetime "updated_at",                               null: false
+    t.integer  "comments_count", limit: 4,     default: 0, null: false
   end
 
   add_index "posts", ["topic_id"], name: "index_posts_on_topic_id", using: :btree
