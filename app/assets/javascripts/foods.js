250ca4820820dc74c3d17dc636c181fff5493f2a
ynzkai <ynzkai@gmail.com>
2016-03-24 23:51:37 +0800

    food show page

--- app/assets/javascripts/foods.js
+++ app/assets/javascripts/foods.js
@@ -1,2 +1,24 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+
+$(document).ready(function() {
+  PicSlider().init("#shopfoods");
+});
+$(document).on("page:change", function() {
+});
+
+var PicSlider = function() {
+
+  return {
+	init: function(container, opts) {
+	  for(var o in opts) {
+		  options[o] = opts[o];
+	  }
+
+      $(container).css("overflow", "hidden");
+      $(container + ">div").css("width", "9999px");
+
+	}
+  };
+};
